a5b388465140cbbd1a418c029a6b5825e28f6e12
Adrien Joly <adrien@whyd.com>
2016-05-05 16:07:22 +0200

    display exercise variant based on student/user id

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -14,6 +14,12 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   var PAGE_TITLE = 'Javascript Exo';
   var FIREBASE_URL = 'https://js-exo-algo.firebaseio.com';
 
+  var NB_VARIANTS = 3;
+
+  function getExerciseMdFile(variant) {
+    return './data/exercice.variant.' + variant + '.json.md';
+  }
+
   /* firebase security rules: {
    "rules": {
       ".read": false,
@@ -23,6 +29,21 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
           "$key": {
            ".write": "newData.exists()" // prevents deletion, cf http://stackoverflow.com/questions/29466247/in-firebase-what-security-rules-should-i-write-to-allow-only-push-to-object
   */
+
+
+  // fixed version of variant3() => returns 0, 1 or 2, depending on the value of number
+  function getVariantByStudentId (id) {
+    // modulo that also works for big integers
+    var modulo = function(divident, divisor) {
+        var partLength = 10;
+        while (divident.length > partLength) {
+            var part = divident.substring(0, partLength);
+            divident = (part % divisor) +  divident.substring(partLength);          
+        }
+        return divident % divisor;
+    };
+    return modulo(id, NB_VARIANTS);
+  };
 
   // Grab a reference to our auto-binding template and give it some initial binding values
   var app = document.querySelector('#app');
@@ -41,6 +62,7 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   };
 
   app.title = PAGE_TITLE;
+  app.questions = [];
   app.loggedIn = false; // init default value, to be set by google-signin
   app.user = null;
   app.backend = null; // Firebase instance
@@ -69,6 +91,15 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 
   function onLogin(userData, offline) {
     app.user = userData;
+    // generate exercise variant, based on student id
+    var variant = getVariantByStudentId(userData.id);
+    app.set('questions', [
+      {
+        i: 1,
+        id: 'code1',
+        mdFile: getExerciseMdFile(variant),
+      },
+    ]);
     if (offline) return;
     var userHash = userData.email.split('@')[0].replace(/[^\w]/g, '_');
     app.backend = new Firebase(FIREBASE_URL + '/submissions/' + userHash);
@@ -112,7 +143,7 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   // FOR PUBLIC TESTING: fakes Google Login
   if (PUBLIC_TEST_MODE) {
     onLogin({
-      id: 1,
+      id: Math.floor(NB_VARIANTS * Math.random()), // variant is based on user id => randomize it
       name: 'Demo User',
       email: 'demo-user@example.com',
       token: 'XXX'
