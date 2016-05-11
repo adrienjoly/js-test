5ad611c5f73a5a2f84005cdcb5bbca0e6fd751a1
Adrien Joly <adrien@whyd.com>
2016-05-11 20:24:25 +0200

    reduced size of errors in complete log files

--- evaluer-classes.sh
+++ evaluer-classes.sh
@@ -3,7 +3,7 @@
 for f in ./classe*.json;
 do
   echo Evaluating $f...
-  node evaluateFile.js $f &>$f.eval.txt
+  node evaluateFile.js $f 2>/dev/null >$f.eval.txt
   echo - wrote $f.eval.txt
   grep SCORE $f.eval.txt >$f.eval.csv
   echo - wrote $f.eval.csv
