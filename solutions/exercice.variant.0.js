999dfd6a78e6deb713735140d9fd207e7eed5719
Adrien Joly <adrien@whyd.com>
2016-05-13 14:15:16 +0200

    added message and solution

--- solutions/exercice.variant.0.js
+++ solutions/exercice.variant.0.js
@@ -1,17 +1,17 @@
 // 1) Inviter l'utilisateur à saisir un nombre
 
-var nb = prompt();
+var nb = prompt('entrez un nombre');
 
 // 2) Afficher première multiplication
 
-console.log(nb, '* 2 =', nb * 2);
+console.log(nb + ' * 2 = ' + nb * 2);
 
 // 3 et 4) Afficher autres multiplications, en utilisant la fonction demandée
 
-function multi(i, j) {
-  return i + ' * ' + j + ' = ' + (i * j);
+function multiplication(nb, coef) {
+  return nb + ' * ' + coef + ' = ' + (nb * coef);
 }
 
 for (var i = 3; i <= 100; ++i) {
-  console.log(multi(nb, i));
+  console.log(multiplication(nb, i));
 }
