44d3b8c009a9db8059645bdd2cdd9648c1745c11
Adrien Joly <adrien@whyd.com>
2016-05-15 19:10:25 +0200

    evaluate.js computes the score for quizzes

--- src/evaluate.js
+++ src/evaluate.js
@@ -1,10 +1,13 @@
-// this script loads and evaluates answers submitted by students from Firebase
+// This script loads and evaluates answers submitted by students from Firebase.
 
 var async = require('async');
 var Firebase = require('firebase');
-var evaluateStudent = require('./exercice.evaluator.js');
+var QuizzEvaluator = require('./QuizzEvaluator.js');
 
 var FIREBASE_URL = 'https://js-exam.firebaseio.com';
+var SOLUTIONS_FILE = './ex.1.quizz.solutions.json';
+
+// helpers
 
 function forEachChild(endpointUrl, handler, callback) {
   var q = async.queue(handler, 1); // handler(task, callnext) will be called for each child
@@ -23,6 +26,22 @@ function forEachChild(endpointUrl, handler, callback) {
     });
   });
 }
+
+// evaluation logic
+
+var quizzEvaluator = new QuizzEvaluator().readSolutionsFromFile(SOLUTIONS_FILE);
+// TODO: also instanciate code evaluator
+
+function evaluateStudent(student, next) {
+  console.log('Evaluating', student.key, '(' + student._uid + ')', '...');
+  var res = quizzEvaluator.evaluateAnswers(student);
+  console.log('=> quizz score:', res.score, '/', res.length);
+  console.log();
+  // TODO: also evaluate code exercises
+  next();
+}
+
+// actual script
 
 forEachChild(FIREBASE_URL + '/submissions', evaluateStudent, function done() {
   process.exit();
