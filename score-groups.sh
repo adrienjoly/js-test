466c4909a2db1177f1516488a02c72b22a056539
Adrien Joly <adrien@whyd.com>
2016-06-17 15:53:33 +0200

    re-evaluated all student scores after bug fix, starting with douglas instance (because it starts with my own answers)

--- score-groups.sh
+++ score-groups.sh
@@ -5,13 +5,13 @@ npm run build
 rm scores.csv
 rm scores.log
 
+echo Evaluating students\' answers from https://douglas.firebaseio.com ...
+npm run eval https://douglas.firebaseio.com >>scores.log 2>&1
+
 echo Evaluating students\' answers from https://linus.firebaseio.com ...
 npm run eval https://linus.firebaseio.com >>scores.log 2>&1
 
 echo Evaluating students\' answers from https://eich.firebaseio.com ...
 npm run eval https://eich.firebaseio.com >>scores.log 2>&1
-
-echo Evaluating students\' answers from https://douglas.firebaseio.com ...
-npm run eval https://douglas.firebaseio.com >>scores.log 2>&1
 
 echo Successfully wrote scores.csv and scores.log files \!
