cc67d7d114e178e4438f24dda53aff699ea64bc6
Adrien Joly <adrien@whyd.com>
2016-06-17 14:55:00 +0200

    refactoring: evaluateStudent() can now be re-used by evaluateFile

--- score-students.sh
+++ score-students.sh
@@ -1,10 +1,12 @@
 # evaluate from email submissions, to log and csv formats (one student mark per line)
 
-mkdir ./student-evals
+cd src
 
-for f in ./student-submissions/*.json;
+for f in ./students/*.json;
 do
-  echo Evaluating $f to ./student-evals/$f.log ...
-  node ./src/evaluateFile.js $f &>./student-evals/$f.log
+  echo Evaluating $f to $f.eval.log ...
+  node ./src/evaluateFile.js $f &>$f.eval.log
 done;
 echo ... and appended students\' grades to ./scores.csv
+
+cd ..
--- src/evaluate.js
+++ src/evaluate.js
@@ -1,20 +1,12 @@
 // This script loads and evaluates answers submitted by students from Firebase.
 
-var fs = require('fs');
 var async = require('async');
 var Firebase = require('firebase');
-var QuizzEvaluator = require('./QuizzEvaluator.js');
-var CodeEvaluator = require('./CodeEvaluator.js');
-
-// outputs
-
-var SCORES_FILE = './scores.csv';
+var evaluateStudent = require('./StudentEvaluator');
 
 // inputs
 
 var FIREBASE_URL = process.argv[2] || 'https://js-exam.firebaseio.com';
-var SOLUTIONS_FILE = './ex.1.quizz.solutions.json';
-var TESTS_FILE = './ex.2.code.tests.json';
 
 // helpers
 
@@ -33,57 +25,6 @@ function forEachChild(endpointUrl, handler, callback) {
       q.push(obj);
       --remaining;
     });
-  });
-}
-
-function setConsolePrefix(prefix) {
-  if (!prefix) {
-    // restore original console
-    console.log = console._log_backup;
-  } else {
-    console._log_backup = console._log_backup || console.log;
-    console.log = function() {
-      for (var i in arguments) {
-        if (arguments[i] instanceof Object || arguments[i] instanceof Array)
-          arguments[i] = sys.inspect(arguments[i]);
-      }
-      console._log_backup(prefix + Array.prototype.join.call(arguments, " ").replace(/\n/g, '\n' + prefix));
-    };
-  }
-}
-
-// evaluation logic
-
-var quizzEvaluator = new QuizzEvaluator().readSolutionsFromFile(SOLUTIONS_FILE);
-var codeEvaluator = new CodeEvaluator().readTestsFromFile(TESTS_FILE);
-
-function evaluateStudent(student, next) {
-  var totalScore = 0;
-  var totalPoints = 0;
-  console.log('\n================================\n')
-  console.log('Evaluating', student.key, '(' + student._uid + ')', '...');
-  console.log('  -  quizz answers:');
-  var quizzAnsw = quizzEvaluator.getAnswerSet(student);
-  setConsolePrefix('  | ');
-  for (var i in quizzAnsw) {
-    console.log(i, ':', quizzAnsw[i]);
-  }
-  setConsolePrefix();
-  var res = quizzEvaluator.evaluateAnswers(student);
-  totalScore += res.score;
-  totalPoints += res.length;
-  console.log('  => quizz score:', res.score, '/', res.length);
-  console.log('  -  code evaluation:');
-  setConsolePrefix('  | ');
-  codeEvaluator.evaluateAnswers(student, function(err, res){
-    totalScore += res.score;
-    totalPoints += res.length;
-    setConsolePrefix();
-    console.log('  => code score:', res.score, '/', res.length);
-    console.log();
-    console.log('=> TOTAL STUDENT SCORE:', totalScore, '/', totalPoints);
-    var csv = [ student.key, totalScore ];
-    fs.appendFile(SCORES_FILE, csv.toString() + '\n', next);
   });
 }
 
