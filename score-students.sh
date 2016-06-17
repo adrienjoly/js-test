24f0332d930d0fb51e6a16a429081559f473d535
Adrien Joly <adrien@whyd.com>
2016-06-17 15:30:52 +0200

    checked that answers submitted by email evaluate into the same scores (diff with result of firebase-based evaluation)

--- score-students.sh
+++ score-students.sh
@@ -1,11 +1,9 @@
 # evaluate from email submissions, to log and csv formats (one student mark per line)
 
-cd src
-
 for f in ./students/*.json;
 do
   echo Evaluating $f to $f.eval.log ...
-  node ./src/evaluateFile.js $f &>$f.eval.log
+  node ./src/evaluateFile.js .$f &>$f.eval.log
 done;
 echo ... and appended students\' grades to ./scores.csv
 
