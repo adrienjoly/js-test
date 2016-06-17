3d9fc6013970c9c031b7186982b8af437fcb5ca6
Adrien Joly <adrien@whyd.com>
2016-06-17 18:44:34 +0200

    lighter evaluation log, by removing redundant error stack traces from jailed

--- score-groups.sh
+++ score-groups.sh
@@ -6,12 +6,12 @@ rm scores.csv
 rm scores.log
 
 echo Evaluating students\' answers from https://douglas.firebaseio.com ...
-npm run eval https://douglas.firebaseio.com >>scores.log 2>&1
+npm run eval https://douglas.firebaseio.com >>scores.log 2>/dev/null
 
 echo Evaluating students\' answers from https://linus.firebaseio.com ...
-npm run eval https://linus.firebaseio.com >>scores.log 2>&1
+npm run eval https://linus.firebaseio.com >>scores.log 2>/dev/null
 
 echo Evaluating students\' answers from https://eich.firebaseio.com ...
-npm run eval https://eich.firebaseio.com >>scores.log 2>&1
+npm run eval https://eich.firebaseio.com >>scores.log 2>/dev/null
 
 echo Successfully wrote scores.csv and scores.log files \!
