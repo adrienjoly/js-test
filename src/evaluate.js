985b3f2fc0a18128e9886d80fd0a927aa9e8e695
Adrien Joly <adrien@whyd.com>
2016-11-30 11:48:17 +0100

    fixed evaluation script, to match firebase new API (v3)

--- src/evaluate.js
+++ src/evaluate.js
@@ -1,19 +1,28 @@
 // This script loads and evaluates answers submitted by students from Firebase.
 
 var async = require('async');
-var Firebase = require('firebase');
+var firebase = require('firebase');
 var evaluateStudent = require('./StudentEvaluator');
 
 // inputs
 
-var FIREBASE_URL = process.argv[2] || 'https://js-exam.firebaseio.com';
+var FIREBASE_URL = process.argv[2] || 'https://js-test-2.firebaseio.com';
 
 // helpers
 
-function forEachChild(endpointUrl, handler, callback) {
+function forEachSubmission(endpointUrl, handler, callback) {
   var q = async.queue(handler, 1); // handler(task, callnext) will be called for each child
   //q.push({name: 'foo', code1: 'console.log("test", 666);'}); // for testing
-  var backend = new Firebase(endpointUrl);
+
+  // TODO: read this info from a config file that will also be compiled for app.js
+  var config = {
+    apiKey: "AIzaSyCBkfcodGHJEJDsnh99KgpP_F3cxU58P9I",
+    databaseURL: endpointUrl,
+    messagingSenderId: "730428017661"
+  };
+  firebase.initializeApp(config);
+
+  var backend = firebase.database().ref('/submissions');
   backend.on('value', function (snapshot) {
     var remaining = snapshot.numChildren();
     q.drain = function() {
@@ -21,7 +30,7 @@ function forEachChild(endpointUrl, handler, callback) {
     };
     snapshot.forEach(function(child) {
       var obj = child.val();
-      obj.key = child.key();
+      obj.key = child.key;
       q.push(obj);
       --remaining;
     });
@@ -31,7 +40,7 @@ function forEachChild(endpointUrl, handler, callback) {
 // actual script
 
 console.log('Reading and evaluating answers from:', FIREBASE_URL, '...');
-forEachChild(FIREBASE_URL + '/submissions', evaluateStudent, function done() {
+forEachSubmission(FIREBASE_URL, evaluateStudent, function done() {
   console.log();
   process.exit();
 });
