78b17f8cecc6ae1eea3d9bc3dafc88d1ea01410f
Adrien Joly <adrien@whyd.com>
2016-12-02 17:56:36 +0100

    evaluator loads firebase config from /exam-data/exam-config.js

--- src/evaluate.js
+++ src/evaluate.js
@@ -6,7 +6,9 @@ var evaluateStudent = require('./StudentEvaluator');
 
 // inputs
 
-var FIREBASE_URL = process.argv[2] || 'https://js-test-2.firebaseio.com';
+var config = require('../exam-data/exam-config.js');
+
+var FIREBASE_URL = process.argv[2] || config.FIREBASE_CONFIG.databaseURL;
 
 // helpers
 
@@ -14,13 +16,10 @@ function forEachSubmission(endpointUrl, handler, callback) {
   var q = async.queue(handler, 1); // handler(task, callnext) will be called for each child
   //q.push({name: 'foo', code1: 'console.log("test", 666);'}); // for testing
 
-  // TODO: read this info from a config file that will also be compiled for app.js
-  var config = {
-    apiKey: "AIzaSyCBkfcodGHJEJDsnh99KgpP_F3cxU58P9I",
-    databaseURL: endpointUrl,
-    messagingSenderId: "730428017661"
-  };
-  firebase.initializeApp(config);
+  if (endpointUrl) {
+    config.FIREBASE_CONFIG.databaseURL = endpointUrl; 
+  }
+  firebase.initializeApp(config.FIREBASE_CONFIG);
 
   var backend = firebase.database().ref('/submissions');
   backend.on('value', function (snapshot) {
