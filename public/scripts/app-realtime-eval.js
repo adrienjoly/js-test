7d7ce372c3af6d19e0138f2a5f9a9bbde47494fd
Adrien Joly <adrien@whyd.com>
2016-12-19 12:46:10 +0100

    auto-eval: compute score and display solutions, on submit

--- public/scripts/app-realtime-eval.js
+++ public/scripts/app-realtime-eval.js
@@ -4,9 +4,43 @@
   // Grab a reference to our auto-binding template and give it some initial binding values
   var app = document.querySelector('#app');
 
+  app._score = null;
+
   app._onLogin = function() {
     app.set('active', true);
+    app.myAnswers._uid = app.user.id;
   };
+
+  // code evaluation
+
+  function computeQuizzScore(studentAnswers, exercise, callback) {
+    var score = 0;
+    for (var qId in exercise.solutions) {
+      if (studentAnswers[qId] === undefined) {
+        score += app.config.quizzGrading.ptsNull;
+      } else if (studentAnswers[qId] == exercise.solutions[qId]) {
+        score += app.config.quizzGrading.ptsRight
+      } else {
+        score += app.config.quizzGrading.ptsWrong;
+      }
+    }
+    callback(null, score);
+  }
+
+  function computeCodeScore(studentAnswers, exercise, callback) {
+    var CodeEvaluator = makeCodeEvaluator(jailed, async, app.config.codeGrading);
+    var codeEvaluator = new CodeEvaluator(exercise.questions);
+    codeEvaluator.evaluateAnswers(studentAnswers, function(err, res){
+      callback(null, res.score);
+    });
+  }
+
+  function computeStudentScores(studentAnswers, callback) {
+    async.mapSeries(app.exercises, function(ex, callback){
+      var computeExerciseScore = ex.isQuizz ? computeQuizzScore : computeCodeScore;
+      computeExerciseScore(studentAnswers, ex, callback);
+    }, callback);
+  }
 
   function evaluateAnswer(id, value) {
     if (app.myAnswers[id] === value) {
@@ -15,10 +49,10 @@
     app.myAnswers[id] = value;
     console.log(app.myAnswers);
     app._toggleLoadingSpinner(id, true);
-    // TODO: evaluate
-    setTimeout(function(){
+    computeStudentScores(app.myAnswers, function(err, res){
+      console.log('=>', err || res);
       app._toggleLoadingSpinner(id, false);
-    }, 200);
+    });
   }
 
   // for quizz questions only
@@ -34,5 +68,32 @@
     var questionId = input.getAttribute('data-id');
     evaluateAnswer(questionId, input.value);
   };
+
+  // when user presses exam submit button
+  app.onSubmitExam = function(evt) {
+    app._toggleButton(document.getElementById('submitConfirmation'), false);
+    computeStudentScores(app.myAnswers, function(err, res){
+      console.log('computeStudentScores =>', err || res);
+      // display expected solutions
+      app.myAnswers = Object.assign({}, app.myAnswers, {
+        _submitted: true,
+        _score: res[0] + res[1], // sum of quizz and code scores
+      });
+      app._toggleButton(document.getElementById('submitConfirmation'), true);
+      app.scrollPageToTop();
+    });
+  };
+
+  // load dependencies for code evaluation
+  app.async(function(){
+    function require(src) {
+      var el = document.createElement('script');
+      el.setAttribute('src', src);
+      document.body.appendChild(el);
+    }
+    require('/scripts/CodeEvaluatorGeneric.js')
+    require('/bower_components/async/lib/async.js');
+    require('/bower_components/jailed/lib/jailed.js');
+  });
 
 })(document);
