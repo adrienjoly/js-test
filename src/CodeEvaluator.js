1cb05c495e8f4befa70ecfaef71bdf2c954acb81
Adrien Joly <adrien@whyd.com>
2016-12-19 11:29:28 +0100

    bugfix: use caolan's async instead of own (buggy) implementation of mapSeries()

--- src/CodeEvaluator.js
+++ src/CodeEvaluator.js
@@ -1,9 +1,10 @@
 var fs = require('fs');
+var async = require('async');
 var jailed = require('jailed-node');
 var config = require('../exam-data/exam-config.js');
 var CodeEvaluatorGeneric = require('../public/scripts/CodeEvaluatorGeneric.js');
 
-var CodeEvaluator = CodeEvaluatorGeneric.makeCodeEvaluator(jailed, config.codeGrading);
+var CodeEvaluator = CodeEvaluatorGeneric.makeCodeEvaluator(jailed, async, config.codeGrading);
 
 CodeEvaluator.prototype.readTestsFromFile = function(filePath) {
   this.tests = JSON.parse(fs.readFileSync(filePath).toString());
