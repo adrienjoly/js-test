41e00a6e47066f5a3dd55748bddd05ea486b53db
Adrien Joly <adrien@whyd.com>
2017-01-02 12:05:39 +0100

    refactoring: use QuizzConverter to parse and render each exercise definition

---
+++ src/QuizzConverter.js
@@ -0,0 +1,56 @@
+// parses and renders a markdown exercise definition
+
+var _ = require('lodash');
+var mustache = require('mustache');
+
+function renderCodeExercise(exerciseData, exNumber) {
+
+  var evalTests = [];
+
+  var questions = exerciseData.renderJsonQuestions().map(function(question, q) {
+    var variants = _.map(question.choices, 'text').map(JSON.parse);
+    variants = variants.length > 0 ? variants : [{}]; // also render coding questions that don't have any variants
+    var exText = question.md;
+    var exEval = question.mdSolution;
+    var exSolution = null;
+    if (exEval) {
+      var parts = exEval.split('\n--\n');
+      exEval = parts.pop(); // evaluation code
+      exEval = exEval.replace(/```js\n*/g, '').replace(/```\n*/g, '');
+      exSolution = parts.pop();
+    }
+    var exerciseData = {
+      i: q + 1, // TODO: prevent id collisions if more than one code.template.md file is used
+      id: 'code' + (q + 1), // TODO: allow each question to override this id
+      variants: variants,
+      testVariants: variants.map(function renderVariant(variantData, i) {
+        return exEval && mustache.render(exEval, variantData);
+      })
+    };
+    evalTests.push(exerciseData);
+    return Object.assign({}, exerciseData, {
+      mdVariants: variants.map(function renderVariant(variantData, i) {
+        return mustache.render(exText, variantData);
+      }),
+      mdSolution: exSolution // TODO: one exSolution per variant? (like for testVariants)
+    });
+    // TODO: obfuscate solution and tests on client-side
+  });
+  
+  return {
+    questions: questions,
+    evalTests: evalTests,
+  };
+}
+
+function renderQuizzExercise(exerciseData, exNumber) {
+  return {
+    questions: exerciseData.renderJsonQuestions(),
+    solutions: exerciseData.getSolutions(),
+  };
+}
+
+module.exports = {
+  renderCodeExercise: renderCodeExercise,
+  renderQuizzExercise: renderQuizzExercise,
+};
