cea63f069bebdcc9c49f6a88f20c685c7b97c8dd
Adrien Joly <adrien@whyd.com>
2017-01-02 18:13:13 +0100

    `npm test` runs on more than 1 variant, for #11

--- src/test-solutions.js
+++ src/test-solutions.js
@@ -11,7 +11,7 @@ var PATH_SOURCE = './exam-data/';
 // 1) render solutions from exercise definition files
 
 var converters = {
-  code: QuizzConverter.renderCodeExercise, // TODO: add support for variants
+  code: QuizzConverter.renderCodeExercise,
   quizz: QuizzConverter.renderQuizzExercise,
 };
 
@@ -23,38 +23,44 @@ var solutions = Object.assign.apply(Object, [{}].concat(_.map(exercises, 'soluti
 
 // 2) generate fake submissions (automated tests)
 
-var _uid = 0; // change user id value in order to test different variants
+var uids = [ 0, 1 ]; // change user id value in order to test different variants
+// TODO: to compute, based on common denominator of all variants defined in exercises
 
 var submissionSet = {
   _ALLEMPTY_: {},
   _ALLGOOD_: solutions
 };
 
-var submissions = Object.keys(submissionSet).map(function(key){
-  return _.extend(submissionSet[key], {
-    key: key,
-    _uid: _uid,
+// combine submissionSet with variants
+var submissions = _.flatten(Object.keys(submissionSet).map(function(key){
+  return uids.map(function(uid) {
+    return Object.assign({}, submissionSet[key], {
+      key: key + uid,
+      _uid: uid,
+    });
   });
-});
+}));
 
 // 3) evaluate fake submissions, and print to standard output
 
-function applyVariant(subm, variant) {
+function applyUserVariant(subm) {
   var fixedSubm = Object.assign({}, subm); // clone object
   // replace array-typed properties by picking one, based on variant index
   for (var exKey in fixedSubm) {
     var exVal = fixedSubm[exKey];
     if (typeof exVal === 'object' && typeof exVal.map === 'function') {
-      fixedSubm[exKey] = exVal[variant];
+      var exVariants = exVal;
+      var variant = evaluateStudent.getVariantByStudentId(subm._uid, exVariants);
+      fixedSubm[exKey] = exVariants[variant];
     }
   }
   return fixedSubm;
 }
 
-function applyVariantThen(variant, asyncFct) {
-  return function(param, callback) {
-    asyncFct(applyVariant(param, variant), callback);
+function applyVariantThen(asyncFct) {
+  return function(subm, callback) {
+    asyncFct(applyUserVariant(subm), callback);
   };
 }
 
-async.mapSeries(submissions, applyVariantThen(_uid, evaluateStudent), process.exit);
+async.mapSeries(submissions, applyVariantThen(evaluateStudent), process.exit);
