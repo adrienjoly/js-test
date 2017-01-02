1c66eb024f3318da666451aad7466f614ca4aad6
Adrien Joly <adrien@whyd.com>
2017-01-02 18:54:00 +0100

    `npm test` evaluates solutions for all possible variants (combination of all exercise questions), closes #11.

--- src/test-solutions.js
+++ src/test-solutions.js
@@ -21,19 +21,48 @@ var exercises = QuizzEnumerator.parseAllFrom(PATH_SOURCE).map(function(exData) {
 
 var solutions = Object.assign.apply(Object, [{}].concat(_.map(exercises, 'solutions')));
 
-// 2) generate fake submissions (automated tests)
+// 1b) computer number of all possible variants, based on exercise definition files
 
-var uids = [ 0, 1 ]; // change user id value in order to test different variants
-// TODO: to compute, based on common denominator of all variants defined in exercises
+var nbVariants = (function(){
+  // Least Common Mulitple, source: http://www.w3resource.com/javascript-exercises/javascript-math-exercise-11.php
+  function lcm_more_than_two_numbers(input_array) {  
+    if (toString.call(input_array) !== "[object Array]")    
+      return  false;    
+    var r1 = 0, r2 = 0;  
+    var l = input_array.length;  
+    for(i=0;i<l;i++) {  
+      r1 = input_array[i] % input_array[i + 1];  
+      if(r1 === 0) {  
+        input_array[i + 1] = (input_array[i] * input_array[i+1]) / input_array[i + 1];  
+      }  
+      else {  
+        r2 = input_array[i + 1] % r1;  
+        if(r2 === 0) {  
+          input_array[i + 1] = (input_array[i] * input_array[i + 1]) / r1;  
+        }  
+        else {  
+          input_array[i+1] = (input_array[i] * input_array[i + 1]) / r2;  
+        }  
+      }  
+    }  
+    return input_array[l - 1];  
+  }  
+  var variants = _.map(_.flatten(_.map(exercises, 'questions')), 'variants').map(function(varArray) {
+    return (varArray || [ {} ]).length;
+  });
+  return lcm_more_than_two_numbers(variants);
+})();
+
+// 2) generate fake submissions (automated tests), based on solutions and variants
 
 var submissionSet = {
   _ALLEMPTY_: {},
   _ALLGOOD_: solutions
 };
 
-// combine submissionSet with variants
+// combine submissionSet with variants (linked to user id)
 var submissions = _.flatten(Object.keys(submissionSet).map(function(key){
-  return uids.map(function(uid) {
+  return Array.apply(null, Array(nbVariants)).map(function(dummy, uid) {
     return Object.assign({}, submissionSet[key], {
       key: key + uid,
       _uid: uid,
