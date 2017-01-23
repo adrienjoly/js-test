faf524fdfaa1400f7c1b647631c314eb5acff825
Adrien Joly <adrien@whyd.com>
2017-01-23 10:35:36 +0100

    fixed templated evaluation script

--- exam-data/eval.sh
+++ exam-data/eval.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# (forked from https://github.com/adrienjoly/js-exam/blob/master/deploy.sh)
-
 files=( package.json ./exam-data/exam-config.js )
 placeholder="__INSTANCE__NUMBER__"
 replacement=1
@@ -9,13 +7,14 @@ replacement=1
 for f in "${files[@]}"
 do
   echo "- replacing mentions of $placeholder in file: $f"
-  sed "s/$placeholder/$replacement/g" $f > $f
+  sed -i.bak "s/$placeholder/$replacement/g" $f
+  rm $f.bak
 done
 
 # npm run eval-student-groups
 src/score-student-groups.sh ./student-data/*.json
 
-git reset --soft HEAD~1
-
 echo "- restoring mentions to $placeholder"
+git reset --soft HEAD~1
+git pull
 git reset --hard
