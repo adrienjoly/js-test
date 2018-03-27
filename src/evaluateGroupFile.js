702ea6c55a80858379d2bd46b41c8a700c6d7bc3
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2018-03-27 20:28:42 +0200

    v2.9.2: grade distribution chart rendering for groups

--- src/evaluateGroupFile.js
+++ src/evaluateGroupFile.js
@@ -5,12 +5,14 @@ var fs = require('fs');
 var _ = require('lodash');
 var async = require('async');
 var evaluateStudent = require('./StudentEvaluator');
+var renderDistributionChart = require('./renderDistributionChart');
 
 var filePath = process.argv[2] || '../student-groups/js-controle-1-classe-1.json';
 
 var PATH_SOURCE = './exam-data/';
 var SCORES_FILE = PATH_SOURCE + 'scores.csv';
 var SCORES_DETAIL_FILE = PATH_SOURCE + 'scores-detail.csv';
+var SCORES_CHART_FILE = PATH_SOURCE + 'scores-chart.txt';
 
 function appendScoreValues(array) {
   var csvLine = array.toString() + '\n';
@@ -39,14 +41,18 @@ var submissions = Object.keys(submissionSet).map(function(key){
   return _.extend(submissionSet[key], { key: key });
 });
 
-async.mapSeries(submissions, evaluateStudent, function(err, res){
+async.mapSeries(submissions, evaluateStudent, function(err, res) {
   if (err) throw err;
+  // list students' total scores
   var flatScores = res.map(function(student) {
     return student.studentTotalScore;
   });
+  // compute average and median
   [
     [ '(AVERAGE)', flatScores.reduce(sum) / res.length ],
     [ '(MEDIAN)', median(flatScores) ]
   ].map(appendScoreValues);
+  // generate grade/score distribution chart
+  fs.appendFileSync(SCORES_CHART_FILE, renderDistributionChart({ flatScores }));
   process.exit();
 });
---
+++ src/renderDistributionChart.js
@@ -0,0 +1,32 @@
+const wunderbar = require('@gribnoysup/wunderbar');
+
+const gradeStep = 3;
+const gradeLines = [0, 3, 6, 9, 12, 15, 18];
+const gradePadding = Math.max.apply(null, gradeLines).toString().length;
+const rangePadding = gradePadding * 2 + 1 + 1; // number of characters to display range
+
+// generate grade/score distribution chart
+function renderDistributionChart({ flatScores }) {
+  const nbStudentsPerGrade = gradeLines.map(function(grade) {
+    return flatScores.reduce(function(count, studentScore) {
+      return count + (studentScore >= grade && studentScore < grade + gradeStep ? 1 : 0);
+    }, 0);
+  });
+  const { chart, scale } = wunderbar(nbStudentsPerGrade, {
+    min: 0,
+    max: flatScores.length, // number of students
+    length: 42,
+  });
+  return []
+    .concat(chart.split('\n').map(function(line, i) {
+      var low = Math.floor(i * gradeStep).toString().padStart(gradePadding, '0');
+      var high = Math.floor((i + 1) * gradeStep - 1).toString().padStart(gradePadding, '0');
+      var range = [low, high].join('-');
+      return range.padEnd(rangePadding, ' ') + line
+        + ' (' + nbStudentsPerGrade[i] + ' stud.)';
+    }))
+    .concat([ ''.padStart(rangePadding, ' ') + scale ])
+    .join('\n');
+}
+
+module.exports = renderDistributionChart;
