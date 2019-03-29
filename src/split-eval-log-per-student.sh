66cfff3f6bd1db0a462c7f5e1b9cb729f4ce17b9
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2019-03-29 12:15:51 +0100

    fix: prevent invalid character names in student eval output files

--- src/split-eval-log-per-student.sh
+++ src/split-eval-log-per-student.sh
@@ -3,7 +3,7 @@
 # Splits a given eval.log file to 1 file per student (./Eval_<student_name>.txt).
 
 perl -pwe '
-  if (/^STUDENT: (.+)/) {
+  if (/^STUDENT: (\S+)/) {
     open $out, ">", "Eval_$1.txt" or die $!;
     select $out;
   }' $1
