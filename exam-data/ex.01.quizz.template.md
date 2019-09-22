140a3ae6d11e49d22e4aa0d7c8eddbac1fd28269
Adrien Joly <adrien.joly+bitbucket@gmail.com>
2019-09-22 15:53:34 +0200

    import exam data from 2a-tc-rendu

---
+++ exam-data/ex.01.quizz.template.md
@@ -0,0 +1,44 @@
+# QCM (1 point par bonne réponse)
+
+Je viens de "cloner" un projet Node.js depuis GitHub. Comment installer les dépendances de ce projet ?
+
+- npm start
+- npm init
+* npm install
+- npm install express --save
+
+---
+
+J'ai créé une API avec Node.js et l'ai lancée en local avec la commande `npm start`. Quel logiciel exécute le code permettant de répondre aux requêtes reçues par cette API ?
+
+- Mon navigateur Web
+- La commande curl
+- Heroku
+* node
+
+---
+
+À quoi sert Express.js ?
+
+- à mettre une API en production
+* à faciliter la création d'un serveur HTTP
+- à accelérer la vitesse de traitement des requêtes HTTP
+- à gérer des requêtes asynchrones comme si elles étaient synchrones
+
+---
+
+Laquelle de ces phrases est CORRECTE ?
+
+* MongoDB est une base de données NoSQL constituée de collections et de documents
+- MongoDB est une base de données NoSQL constituée de tables et de lignes
+- MongoDB est une base de données SQL constituée de collections et de documents
+- MongoDB est une base de données SQL constituée de tables et de lignes
+
+---
+
+Laquelle de ces phrases est INCORRECTE ?
+
+- await permet d'attendre qu'une Promise soit résolue
+- await ne peut être utilisé que depuis une fonction async
+- sans await, deux fonctions asynchrones peuvent être exécutées en parallèle
+* await permet d'exécuter une fonction synchrone comme si elle était asynchrone
---
+++ exam-data/exam-config.js
@@ -0,0 +1,59 @@
+/*
+function readfile(filePath) {
+  return require('fs').readFileSync(filePath).toString();
+}
+*/
+
+module.exports = {
+
+  // Front-end config
+  title: 'Partiel Node.js 2A TC',
+
+  // General settings
+  PUBLIC_TEST_MODE: false, // set to false to restrict acccess and identify students using Google Login
+  DISPLAY_SOLUTIONS_AFTER_SUBMIT: false, // set to false, for real exams
+
+  redirectToHttps: true,
+
+  // Settings for conversion and publication of exercise templates
+  examPack: {
+    publishSolutions: false, // `true` required for realtime-eval/auto-eval back-ends, DISPLAY_SOLUTIONS_AFTER_SUBMIT and/or dashboard
+    publishEvalTests: false, // `true` required for realtime-eval/auto-eval back-ends, DISPLAY_SOLUTIONS_AFTER_SUBMIT and/or dashboard
+  },
+
+  // Back-end config
+  backend: {
+    type: 'firebase', // 'realtime-eval', 'auto-eval', or 'firebase' (with FIREBASE_CONFIG)
+    /*
+    EMAIL_SUBMIT_CONFIG: {
+      mdTemplate: readfile('public/data/submitted.md'),
+    },
+    */
+    FIREBASE_CONFIG: {
+      apiKey: "AIzaSyDkiphR3IUCQ4CcjUgW6-q9ryYc9MxwVs0",
+      authDomain: "js-ps2.firebaseapp.com",
+      databaseURL: "https://js-ps2.firebaseio.com",
+      projectId: "js-ps2",
+      messagingSenderId: "18232771090"
+      // => back-office: https://console.firebase.google.com/project/js-ps2/database/data
+    },
+  },
+
+  teacherEmail: 'adrien.joly@eemi.com', // required for dashboard auth
+  
+  // Authentication
+  GOOGLE_CLIENT_ID: '247219641427-ifeq88p7rgor9al5ksduds7ug0ba7djr.apps.googleusercontent.com', // generated from https://console.developers.google.com/apis/credentials?project=eemi-own-exam&authuser=1
+  GOOGLE_CLIENT_DOMAIN: 'eemi.com', // to restrict access to users from a certain domain only
+  LOGIN_INVITE: 'Connectez-vous à l\'aide de votre compte Google de l\'EEMI:',
+  
+  // Evaluation / grading
+  quizzGrading: {
+    ptsRight: 1,
+    ptsWrong: 0, // or -0.5 for example
+    ptsNull: 0,
+  },
+  codeGrading: {
+    ptsPerExercise: 3.75,
+  }
+
+};
