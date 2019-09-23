decf213670d3b449e2e735e8d3d72214b9dfa09a
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2019-09-23 18:30:43 +0200

    generate stats

--- src/eval-student-submissions.sh
+++ src/eval-student-submissions.sh
@@ -19,4 +19,18 @@ do
 done;
 
 mv exam-data/score*.* $EVAL_PATH/
+
+# render distribution chart
+node -e "require('./src/renderDistributionChart').renderFromScoreFileStream({}, console.log);" \
+  < $EVAL_PATH/scores-detail.csv \
+  > $EVAL_PATH/scores-chart.txt
+
+# append statistics to scores-detail.csv
+node -e "require('./src/computeStats').renderCsvLinesFromScoreFileStream({}, console.log);" \
+  < $EVAL_PATH/scores-detail.csv \
+  >> $EVAL_PATH/scores-detail.csv
+
+# pad each csv column with spaces, for better lisibility
+column -t -s "," $EVAL_PATH/scores-detail.csv > $EVAL_PATH/scores-detail.txt
+
 echo "✅  Done!"
