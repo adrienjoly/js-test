8853bc5f34c7d61de5f3688e78902f503b965312
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-03-18 15:19:29 +0100

    fix: prevent getting more than one eval file per student

--- src/eval-student-submissions.sh
+++ src/eval-student-submissions.sh
@@ -10,27 +10,27 @@ rm ./exam-data/score*.* &>/dev/null
 for FILEPATH in $*;
 do
   EVAL_PATH=exam-data/email-submissions
-  FILENAME=$(basename "$FILEPATH")
+  FILENAME=$(basename "${FILEPATH}")
   STUDENT_NAME="${FILENAME%.*}"
 
-  echo "* Evaluating $FILEPATH to $EVAL_PATH/ ..."
-  mkdir $EVAL_PATH &>/dev/null
-  node ./src/evaluateStudentFile.js ".$FILEPATH" >$EVAL_PATH/Eval_$STUDENT_NAME.txt
+  echo "* Evaluating ${FILEPATH} to ${EVAL_PATH}/ ..."
+  mkdir ${EVAL_PATH} &>/dev/null
+  node ./src/evaluateStudentFile.js "../${FILEPATH}" >${EVAL_PATH}/Eval_${STUDENT_NAME}.txt
 done;
 
-mv exam-data/score*.* $EVAL_PATH/
+mv exam-data/score*.* ${EVAL_PATH}/
 
 # render distribution chart
 node -e "require('./src/renderDistributionChart').renderFromScoreFileStream({}, console.log);" \
-  < $EVAL_PATH/scores-detail.csv \
-  > $EVAL_PATH/scores-chart.txt
+  < ${EVAL_PATH}/scores-detail.csv \
+  > ${EVAL_PATH}/scores-chart.txt
 
 # append statistics to scores-detail.csv
 node -e "require('./src/computeStats').renderCsvLinesFromScoreFileStream({}, console.log);" \
-  < $EVAL_PATH/scores-detail.csv \
-  >> $EVAL_PATH/scores-detail.csv
+  < ${EVAL_PATH}/scores-detail.csv \
+  >> ${EVAL_PATH}/scores-detail.csv
 
 # pad each csv column with spaces, for better lisibility
-column -t -s "," $EVAL_PATH/scores-detail.csv > $EVAL_PATH/scores-detail.txt
+column -t -s "," ${EVAL_PATH}/scores-detail.csv > ${EVAL_PATH}/scores-detail.txt
 
 echo "✅  Done!"
